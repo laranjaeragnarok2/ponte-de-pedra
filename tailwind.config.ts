c3e2e9b5636f114ddbb95619c43f18b33d0ecfa4
Arthur Horyu <1horyuarthur@gmail.com>
2025-07-04 22:50:16 +0000

    Sugestão de combinações de fontes
    Com certeza! É fundamental que as f

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['Montserrat', 'sans-serif'],
+        body: ['Lato', 'sans-serif'],
         headline: ['"Playfair Display"', 'serif'],
         code: ['monospace'],
       },
